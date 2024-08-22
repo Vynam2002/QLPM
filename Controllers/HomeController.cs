00d54ff0155eed5a5bd1ddfa00e922b7628d3cb8
Vynam2002 <vithanhnamckpt2002@gmail.com>
2024-08-22 22:47:47 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using QLPM.Models;
+
+namespace QLPM.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+ [HttpPost]
+    public IActionResult Index(string MaSinhVien, string HoTen)
+    {
+        string strOutput = "Mã sinh viên là: " + MaSinhVien + "tên là: " + HoTen;
+        ViewBag.Message = strOutput;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
